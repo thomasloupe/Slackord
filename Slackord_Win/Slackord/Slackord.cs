98c89c60d509fefdb5323d4d57b1547be68f0008
thomasloupe <6563450+thomasloupe@users.noreply.github.com>
2022-09-07 09:18:57 -0500

    version bump

--- Slackord_Win/Slackord/Slackord.cs
+++ Slackord_Win/Slackord/Slackord.cs
@@ -15,7 +15,7 @@ namespace Slackord
 {
     public partial class Slackord : MaterialForm
     {
-        private const string CurrentVersion = "v2.2.5";
+        private const string CurrentVersion = "v2.2.5.1";
         private DiscordSocketClient _discordClient;
         private OpenFileDialog _ofd;
         private string _discordToken;
