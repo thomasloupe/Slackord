8ff9616da34523f97ca29021cdd6f6b3040dbd1d
thomasloupe <6563450+thomasloupe@users.noreply.github.com>
2024-04-01 23:42:46 -0500

    4.3.1 b1
    Remove some Mac components and update packages.

--- Slackord/MauiProgram.cs
+++ Slackord/MauiProgram.cs
@@ -9,16 +9,12 @@ namespace Slackord
             MauiAppBuilder builder = MauiApp.CreateBuilder();
             _ = builder
                 .UseMauiApp<App>()
-                // Initialize the .NET MAUI Community Toolkit by adding the below line of code
                 .UseMauiCommunityToolkit()
-                // After initializing the .NET MAUI Community Toolkit, optionally add additional fonts
                 .ConfigureFonts(fonts =>
                 {
                     _ = fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     _ = fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
-
-            // Continue initializing your .NET MAUI App here
 
             return builder.Build();
         }
